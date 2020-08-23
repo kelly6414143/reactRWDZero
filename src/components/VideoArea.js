90a8306e5f79659ceba8eb78f2bed2ff71add89a
kelly6414143 <kelly6414143@gmail.com>
2020-08-23 21:32:52 +0800

    移除styled components

--- src/components/VideoArea.js
+++ src/components/VideoArea.js
@@ -2,32 +2,6 @@ import React from "react";
 
 const VideoArea = ({ styles, windowWidth }) => {
 
-//     const coolBoxKeyframes = keyframes`
-//     0% {
-//       height: 0px;
-//       background: green;
-//     }
-//     100% {
-//       height: 200px;
-//       background: blue;
-//     }
-//   `
-
-//     const CoolBox = styled.div`
-//     display: inline-block;
-//     background: green;
-//     width: 100px;
-//     position: relative;
-//     animation-name: ${coolBoxKeyframes};
-//     animation-duration: 2s;
-//     animation-timing-function: ease;
-//     animation-delay: 0s;
-//     animation-iteration-count: 1;
-//     animation-direction: normal;
-//     animation-fill-mode: forwards;
-//     animation-play-state: running;
-//   `
-
     const VideoAreaStyle = {
         display: "flex",
         width: "calc(100% - 40px)",
@@ -104,7 +78,6 @@ const VideoArea = ({ styles, windowWidth }) => {
                             <span></span>
                         </div>
                     </div>
-                    {/* <Box /> */}
                 </div>
             </div>
 
