a6696a2b6fa9ccfb1e580e2dfffb463aef7303ef
kelly6414143 <kelly6414143@gmail.com>
2021-06-30 22:14:34 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65e5836f5fb1fc5081d8e1a67569c545.js"
+  "/kelly6414143/reactRWDZero/precache-manifest.435dfda56991cbb71032bdd428eceb9f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kelly6414143/reactRWDZero/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
